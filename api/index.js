4293109f3a1b499b7c4482d16f5d518b160ca362
Yatharth Jagdhari <yatharthjagdhariyj@gmail.com>
2024-05-07 16:22:08 +0530

    database connected

--- api/index.js
+++ api/index.js
@@ -1,5 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+//nQilR3gVui601BTw
+//yatharth
+//mongodb+srv://yatharth:<password>@mern-estate.ndnqzmc.mongodb.net/
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!!");
